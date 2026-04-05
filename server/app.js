117d37a87f0fa31efeedbfeee5699c18588f313c
Benjamin Wilberforce <benjaminamonoowilberforce@gmail.com>
2026-04-05 21:03:03 +0000

    feat(api): add auth routes

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import { authRouter } from "./routes/auth.routes.js";
 
 
 const app = express();
 
-app.get("/health", (req, res) => res.send({title: "up and ready."}))
+app.get("api/health", (req, res) => res.send({title: "up and ready."}));
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Fiscal Atelier server is running on http://localhost:${PORT}`);
