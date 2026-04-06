3a552a86b2e25fe727559030fbc8674d01116b8c
Benjamin Wilberforce <benjaminamonoowilberforce@gmail.com>
2026-04-06 22:26:54 +0000

    feat(api): add built-in express middlewares

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
 import { connectToDatabase } from "./database/mongodb.js";
 import { authRouter } from "./routes/auth.routes.js";
@@ -8,6 +9,10 @@ import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser());
 
 app.get("/api/health", (req, res) => res.send({title: "up and ready."}));
 app.use("/api/auth", authRouter);
