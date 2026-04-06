81b070adbebb371acf2e8f2ebe94980ba2da6d9d
Benjamin Wilberforce <benjaminamonoowilberforce@gmail.com>
2026-04-06 22:22:08 +0000

    feat(api): add error middleware

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import { connectToDatabase } from "./database/mongodb.js";
 import { authRouter } from "./routes/auth.routes.js";
 import { userRouter } from "./routes/user.routes.js";
 import { transactionRouter } from "./routes/transaction.routes.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 
 const app = express();
@@ -12,6 +13,8 @@ app.get("/api/health", (req, res) => res.send({title: "up and ready."}));
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/transactions", transactionRouter);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
   console.log(`Fiscal Atelier server is running on http://localhost:${PORT}`);
